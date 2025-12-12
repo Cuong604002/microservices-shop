397b6b280b949c00abcd33abc1b7b24d95f7d386
Cuong604002 <vuuongqwer@gmail.com>
2025-12-12 17:46:49 +0700

    123

---
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/service/InventoryService.java
@@ -0,0 +1,40 @@
+package com.programmingtechie.inventoryservice.service;
+
+import com.programmingtechie.inventoryservice.dto.InventoryResponse;
+import com.programmingtechie.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.programmingtechie.inventoryservice.dto.InventoryRequest;
+import com.programmingtechie.inventoryservice.model.Inventory;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    @SneakyThrows
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("Checking Inventory");
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                        InventoryResponse.builder()
+                                .skuCode(inventory.getSkuCode())
+                                .isInStock(inventory.getQuantity() > 0)
+                                .build()
+                ).toList();
+    }
+    public void createInventory(InventoryRequest inventoryRequest) {
+        Inventory inventory = new Inventory();
+        inventory.setSkuCode(inventoryRequest.getSkuCode());
+        inventory.setQuantity(inventoryRequest.getQuantity());
+
+        inventoryRepository.save(inventory);
+    }
+}
